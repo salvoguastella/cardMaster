48332acf64e4b0c52f95ac81cc3e14bebcd17c52
Salvo <whaaagh@gmail.com.com>
2017-09-27 12:54:05 +0100

    Icon box now shows if an icon is already assigned to a card

--- resources/php_scripts/getImageList.php
+++ resources/php_scripts/getImageList.php
@@ -10,55 +10,81 @@
 
 	$limit = $page*$page_size - $start;
 
-	$countSQL = "SELECT id,name,path FROM icons";
+	$usedIconsSQL = "SELECT image, name FROM `card_data` JOIN `card_it` ON `card_data`.id = `card_it`.ref GROUP BY image";
 
 	try{
 
-		$stm=$conn->prepare($countSQL);
+		$stm=$conn->prepare($usedIconsSQL);
 		$stm->execute();
-		$totalIcons = $stm->rowCount();
-		$totalPages = ceil($totalIcons/$page_size);
 
-		$SQL="SELECT id,name,path FROM icons ORDER BY name LIMIT $start,$limit";
+		$usedIcons = [];
+		$usedIconsNames = [];
+
+		while($row=$stm->fetchObject()){
+			array_push($usedIcons, $row->image);
+			$usedIconsNames[$row->image] = array("name" => $row->name);
+		}
+
+		$countSQL = "SELECT id,name,path FROM icons";
 
 		try{
 
-			$stm=$conn->prepare($SQL);
+			$stm=$conn->prepare($countSQL);
 			$stm->execute();
+			$totalIcons = $stm->rowCount();
+			$totalPages = ceil($totalIcons/$page_size);
 
-			$imageList = "";
-			while($row=$stm->fetchObject()){
-				//for flags HERE. FLAG FILTERING IS DONE ON RESULTS
-				$imageList .= "<img class='iconImage' data-id='".$row->id."' src='".$row->path."' alt='".$row->name."' title='".$row->name."' >";
+			$SQL="SELECT id,name,path FROM icons ORDER BY name LIMIT $start,$limit";
+
+			try{
+
+				$stm=$conn->prepare($SQL);
+				$stm->execute();
+
+				$imageList = "";
+				while($row=$stm->fetchObject()){
+					//for flags HERE. FLAG FILTERING IS DONE ON RESULTS
+					if(in_array($row->id, $usedIcons)){
+						$imageList .= "<img class='iconImage used' data-id='".$row->id."' src='".$row->path."' alt='".$row->name."' title='".$row->name.", assigned to ".$usedIconsNames[$row->id]["name"]."' >";
+					}
+					else{
+						$imageList .= "<img class='iconImage' data-id='".$row->id."' src='".$row->path."' alt='".$row->name."' title='".$row->name."' >";
+					}
+				}
+
+				$commandList = "<div class='page' data-index='1'>1</div>";
+				if($page > 1){
+					$commandList.= "<div class='page' data-index='".($page-1)."'><i class='fa fa-angle-left'></i></div>";
+				}
+				if($page > 1 && $page < $totalPages){
+					$commandList.= "<div class='page current' data-index='".$page."'>".$page."</div>";
+				}
+				if($page < $totalPages){
+					$commandList.= "<div class='page' data-index='".($page+1)."'><i class='fa fa-angle-right'></i></div>";
+				}
+				$commandList.= "<div class='page' data-index='".$totalPages."'>".$totalPages."</div>";
+
+				$iconPage = [];
+
+				$iconPage["list"] = $imageList;
+				$iconPage["commands"] = $commandList;
+
+				echo json_encode($iconPage);
+			}
+			catch(PDOException $e) {
+				echo "Error: " . $e->getMessage() . " " .$SQL;
 			}
 
-			$commandList = "<div class='page' data-index='1'>1</div>";
-			if($page > 1){
-				$commandList.= "<div class='page' data-index='".($page-1)."'><i class='fa fa-angle-left'></i></div>";
-			}
-			if($page > 1 && $page < $totalPages){
-				$commandList.= "<div class='page current' data-index='".$page."'>".$page."</div>";
-			}
-			if($page < $totalPages){
-				$commandList.= "<div class='page' data-index='".($page+1)."'><i class='fa fa-angle-right'></i></div>";
-			}
-			$commandList.= "<div class='page' data-index='".$totalPages."'>".$totalPages."</div>";
-
-			$iconPage = [];
-
-			$iconPage["list"] = $imageList;
-			$iconPage["commands"] = $commandList;
-
-			echo json_encode($iconPage);
 		}
 		catch(PDOException $e) {
 			echo "Error: " . $e->getMessage() . " " .$SQL;
 		}
-
 	}
 	catch(PDOException $e) {
 		echo "Error: " . $e->getMessage() . " " .$SQL;
 	}
+
+
 
 
 
